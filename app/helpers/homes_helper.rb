a04c83bdd9627813c8def07eaaceb86c1474ab0f
Wildan <wildan.p@kiranatama.com>
2014-05-13 14:21:40 +0700

    Added histories

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,2 +1,10 @@
 module HomesHelper
+  def popover_history_button(year, placement, title, content)
+    button_tag year, :class => "btn btn-default history_button", 
+      "data-container" => "body", 
+      'data-toggle' => "popover", 
+      "data-placement" => placement, 
+      "title" => title, 
+      "data-content" => content
+  end
 end
